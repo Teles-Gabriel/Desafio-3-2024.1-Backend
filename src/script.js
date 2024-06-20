38d7cc77245aa0cc11e438b6da8eba20e74d5dc7
Gabriel Teles <gabrielteles0609@gmail.com>
2024-06-19 23:27:30 -0300

    requests

--- src/script.js
+++ src/script.js
@@ -1,23 +1,46 @@
-//função de mudar imagem pelo id e pela url
-function changeImage(id, url) {
-  document.getElementById(id).src = url;
-}
-//função de mudar texto pelo id e pelo texto
-function changeText(id, text) {
-  document.getElementById(id).innerText = text;
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292';
+let pokemonList = [];
+let currentIndex = 0;
+
+async function fetchPokemonList() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    pokemonList = data.results;
+    // console.log(pokemonList)
+    showPokemon(currentIndex);
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+async function fetchPokemonDetails(pokemon) {
+    const response = await fetch(pokemon.url);
+    const data = await response.json();
+    return data;
+}
+
+function changeImage(id, url) {
+    document.getElementById(id).src = url;
+}
+
+function changeText(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+async function showPokemon(index) {
+    const pokemon = pokemonList[index];
+    const details = await fetchPokemonDetails(pokemon);
+    changeText('name', details.name);
+    changeImage('img_sprite_front_default', details.sprites.front_default);
+}
 
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+    currentIndex = (currentIndex === 0) ? pokemonList.length - 1 : currentIndex - 1;
+    console.log(currentIndex)
+    showPokemon(currentIndex);
 }
 
+
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+    currentIndex = (currentIndex === pokemonList.length - 1) ? 0 : currentIndex + 1;
+    showPokemon(currentIndex);
 }
+
+fetchPokemonList();
